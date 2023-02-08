813c1190eecb9e0383bc7631042d31daec5b3ee4
BenI570 <benz5673@icloud.com>
2023-02-08 16:59:05 -0500

    Test from Ben Interlichia

--- src/main/java/frc/robot/subsystems/AprilTagPose.java
+++ src/main/java/frc/robot/subsystems/AprilTagPose.java
@@ -3,6 +3,6 @@ package frc.robot.subsystems;
 public class AprilTagPose {
 
     public static void main(String[] args) {
-        System.out.println("This is a test");
+        System.out.println("This is a test, but isn't working");
     }
 }
