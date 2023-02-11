8bfdee369daecccc8e758f0aedca6b11f8014253
BenI570 <benz5673@icloud.com>
2023-02-11 09:47:14 -0500

    VisionObjectAPI has been started, somewhat complete

--- src/main/java/frc/robot/subsystems/VisionObjectAPI.java
+++ src/main/java/frc/robot/subsystems/VisionObjectAPI.java
@@ -5,6 +5,8 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import java.util.Objects;
+
 public class VisionObjectAPI extends SubsystemBase {
     NetworkTableInstance networkTableInstance;
     NetworkTable objectTable;
@@ -14,7 +16,10 @@ public class VisionObjectAPI extends SubsystemBase {
         this.objectTable = this.networkTableInstance.getTable("Object");
     }
 
-    public Translation2d getNearestObject() {
+    public Translation2d getNearestObject(String objectType) {
+        // Two other ways to filter objectType:
+        // Figure out a way to filter out all but a certain objectType when obtaining data from the NetworkTable
+        // Delete those entries and their corresponding x and y values after obtaining the data
 
         var name = this.objectTable.getEntry("Name").getStringArray(null);
         var x = this.objectTable.getEntry("xPos").getDoubleArray(new double[]{});
@@ -23,6 +28,9 @@ public class VisionObjectAPI extends SubsystemBase {
         var maxTranslation = new Translation2d();
 
         for (int i = 0; i < name.length; i++) {
+            if (!Objects.equals(objectType, name[i])) {
+                continue;
+            }
             var translation = new Translation2d(x[i], y[i]);
             var distance = translation.getNorm();
 
