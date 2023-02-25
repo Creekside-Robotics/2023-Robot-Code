b3015df648563d60c7ec1a4921152ab786097f30
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2023-02-25 10:27:56 -0500

    Setting up commands and subsystems

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,17 +6,12 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.DriveToPose;
-import frc.robot.commands.ManualDrive;
+import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -25,30 +20,45 @@ import java.util.List;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  XboxController xboxController = new XboxController(0);
-  private final JoystickButton resetPoseButton = new JoystickButton(xboxController, 3);
-  private final SwerveModule frontRight = new SwerveModule(1, 2, 1);
-  private final SwerveModule frontLeft = new SwerveModule(3, 4, 2);
-  private final SwerveModule backRight = new SwerveModule(5, 6, 3);
-  private final SwerveModule backLeft = new SwerveModule(7, 8, 4);
-  private final VisionPoseAPI poseAPI = new VisionPoseAPI();
-  private final VisionObjectAPI objectAPI = new VisionObjectAPI();
-  private final Drivetrain drivetrain = new Drivetrain(frontRight, frontLeft, backRight, backLeft, poseAPI);
-  private final Arm lowerArm = new Arm(new int[]{9, 10}, 0, 0, new boolean[]{true, true}, true);
-  private final ManualDrive manualDrive = new ManualDrive(drivetrain, xboxController);
-  private final DriveToPose exampleDriveToPose = new DriveToPose(
-          drivetrain,
-          () -> new Pose2d(1, 1, new Rotation2d(Math.PI)),
-          ArrayList::new
-  );
-  private final Command resetPoseCommand = new InstantCommand(
-          () -> drivetrain.setPose(Constants.startingPose)
-  );
+  // Subsystems
+  private XboxController xboxController;
+  private SwerveModule frontRight;
+  private SwerveModule frontLeft;
+  private SwerveModule backRight;
+  private SwerveModule backLeft;
+  private VisionPoseAPI poseAPI;
+  private VisionObjectAPI objectAPI;
+  private Drivetrain drivetrain;
+  private Indexer indexer;
+  private Intake intake;
+  private Arm lowerArm;
+  private Arm upperArm;
+  private Claw claw;
+
+  // Commands
+  private ManualDrive manualDrive;
+  private ResetPose resetPose;
+  private DriveToPose testTrajectory;
+  private SetIndexerMode runIndexerClockwise;
+  private SetIndexerMode runIndexerCounterclockwise;
+  private SetIndexerMode stopIndexer;
+  private SetArmSpeed stopLowerArm;
+  private SetArmSpeed stopUpperArm;
+  private SetArmPosition testSetLowerArm;
+  private SetArmPosition testSetUpperArm;
+  private OpenClaw openClaw;
+  private CloseClaw closeClaw;
+
+  // Buttons
+
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
+    createSubsystems();
+    createCommands();
+    createButtons();
     configureButtonBindings();
   }
 
@@ -58,9 +68,43 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
+
+  private void createSubsystems(){
+    this.xboxController = new XboxController(0);
+    this.frontRight = new SwerveModule(1, 2, 1);
+    this.frontLeft = new SwerveModule(3, 4, 2);
+    this.backRight = new SwerveModule(5, 6, 3);
+    this.backLeft = new SwerveModule(7, 8, 4);
+    this.poseAPI = new VisionPoseAPI();
+    this.objectAPI = new VisionObjectAPI();
+    this.drivetrain = new Drivetrain(frontRight, frontLeft, backRight, backLeft, poseAPI);
+    this.indexer = new Indexer(12, false);
+    this.intake = new Intake(13, 14, false, false, 3, 4, 5, 6);
+    this.lowerArm = new Arm(new int[]{9, 10}, 0, 0, new boolean[]{true, false}, true);
+    this.upperArm = new Arm(new int[]{11}, 1, 0, new boolean[]{false}, true);
+    this.claw = new Claw();
+  }
+
+  private void createCommands(){
+    this.manualDrive = new ManualDrive(this.drivetrain, this.xboxController);
+    this.resetPose = new ResetPose(this.drivetrain, new Pose2d(0, 0, new Rotation2d(0)));
+    this.testTrajectory = new DriveToPose(this.drivetrain, () -> new Pose2d(1, 1, new Rotation2d(Math.PI)), ArrayList::new);
+    this.runIndexerClockwise = new SetIndexerMode(this.indexer, Indexer.Mode.Clockwise);
+    this.runIndexerCounterclockwise = new SetIndexerMode(this.indexer, Indexer.Mode.CounterClockwise);
+    this.stopIndexer = new SetIndexerMode(this.indexer, Indexer.Mode.Stopped);
+    this.stopLowerArm = new SetArmSpeed(this.lowerArm, 0);
+    this.stopUpperArm = new SetArmSpeed(this.upperArm, 0);
+    this.testSetLowerArm = new SetArmPosition(this.lowerArm, 0.25, -0.2, false);
+    this.testSetUpperArm = new SetArmPosition(this.upperArm, 0, 0.2, false);
+    this.openClaw = new OpenClaw(this.claw);
+    this.closeClaw = new CloseClaw(this.claw);
+  }
+
+  private void createButtons(){
+
+  }
+
   private void configureButtonBindings() {
-    this.drivetrain.setDefaultCommand(this.manualDrive);
-    this.resetPoseButton.whenPressed(this.resetPoseCommand);
   }
 
   /**
