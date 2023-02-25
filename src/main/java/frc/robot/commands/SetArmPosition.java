9d6f6dbbf74f7f80033cf9d8b4d7818edf82c4c7
josephhadad05 <91989021+josephhadad05@users.noreply.github.com>
2023-02-24 22:47:27 -0500

    Adding SetArmPosition.java command

---
+++ src/main/java/frc/robot/commands/SetArmPosition.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
+
+// Sets arm to inputted position
+public class SetArmPosition extends CommandBase {
+    /** Creates a new SetArmPosition. */
+    private final Arm arm;
+    private final boolean hold;
+    private final double endPosition;
+    final private double speed = 0.4;
+    final private double tolerance = 0.03;
+
+    public SetArmPosition(Arm arm, double position, boolean hold) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        this.arm = arm;
+        this.hold = hold;
+        this.endPosition = position;
+        addRequirements(this.arm);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        double difference = this.endPosition - this.arm.getPosition();
+        if (Math.abs(difference) < tolerance) {
+            this.arm.setSpeed(0);
+        } else if (difference > 0) {
+            this.arm.setSpeed(this.speed);
+        } else if (difference < 0) {
+            this.arm.setSpeed(-this.speed);
+        }
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return Math.abs(this.endPosition - this.arm.getPosition()) < tolerance && !this.hold;
+    }
+}
