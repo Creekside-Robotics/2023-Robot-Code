b3dd62c4778e8c24421a4593851b04253ad3e952
Joseph Hadad <91989021+josephhadad05@users.noreply.github.com>
2023-03-22 13:02:16 -0400

    Update SetArmPosition.java
    Stopping arms on finish

--- src/main/java/frc/robot/commands/SetArmPosition.java
+++ src/main/java/frc/robot/commands/SetArmPosition.java
@@ -61,7 +61,9 @@ public class SetArmPosition extends CommandBase {
 
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+        this.arm.setSpeed(0);
+    }
 
     // Returns true when the command should end.
     @Override
